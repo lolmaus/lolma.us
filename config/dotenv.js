04a4b69a9942fa4b7ac2dbda7f618d46f521dca4
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2017-12-24 12:51:23 +0300

    Detect `ember deploy prod` as production env

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,12 +1,28 @@
-const fs       = require('fs')
+const fs = require('fs')
 
-const environment = process.env.EMBER_ENV || 'development'
+function deployEnv () {
+  if (
+    process.argv[2] === 'deploy'
+    && (process.argv[3] === 'prod' || process.argv[3] === 'production')
+  ) {
+    return 'production'
+  }
+}
+
+
+
+let environment =
+  process.env.EMBER_ENV
+  || deployEnv()
+  || 'development'
 
 const defaultTarget = environment === 'production' ? 'prod' : 'localhost-4200'
 const target        = process.env.LMS_DEPLOY_TARGET || defaultTarget
 
 const dotEnvFile = `./.env-${target}`
-if (!fs.existsSync(dotEnvFile)) throw new Error(`ember-cli-build.js: dot-env file not found: ${dotEnvFile}`)
+
+if (fs.existsSync(dotEnvFile)) console.info(`using dotenv file: ${dotEnvFile}`)
+else console.warn(`dot-env file not found: ${dotEnvFile}, assuming env vars are passed manually`)
 
 
 
