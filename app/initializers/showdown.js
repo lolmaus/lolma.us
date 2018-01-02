ff7690417d38f4d54570e47eb2fac349398a52fb
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2018-01-02 19:32:45 +0300

    Linkable headers, fixes #16

--- app/initializers/showdown.js
+++ app/initializers/showdown.js
@@ -1,19 +1,23 @@
 import showdown from 'showdown'
 import hljs from 'npm:highlight.js'
 
+
+
+function htmlunencode (text) {
+  return (
+    text
+      .replace(/&amp;/g, '&')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+  )
+}
+
+
+
 export function initialize () {
   showdown.setFlavor('github')
 
   showdown.extension('codehighlight', function () {
-    function htmlunencode (text) {
-      return (
-        text
-          .replace(/&amp;/g, '&')
-          .replace(/&lt;/g, '<')
-          .replace(/&gt;/g, '>')
-      )
-    }
-
     function replacement (wholeMatch, match, left, right) {
       const classesRegex = /class="(.+?)"/
       const hasClasses = classesRegex.test(left)
@@ -45,6 +49,14 @@ export function initialize () {
         const flags = 'g'
         return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags)
       },
+    }
+  })
+
+  showdown.extension('linkable-headers', function () {
+    return {
+      type    : 'output',
+      regex   : /<h(\d?) id="(.+?)">(.+?)<\/h\d?>/g,
+      replace : '<h$1 id="$2" class="headingWithLink"><a href="#$2" class="headingWithLink-link">#</a>$3</h$1>',
     }
   })
 }
